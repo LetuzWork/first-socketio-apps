1e7a4af6b3a4ba6bea4b173220eed7c4633e6bdc
letuzo <martinlaguna1859@gmail.com>
2022-01-08 15:56:39 -0300

    enter to send msgs added from 2nd local repo

--- public/main.js
+++ public/main.js
@@ -6,18 +6,18 @@ const terminal = document.getElementById("terminal");
 const input = document.getElementById("keyboard-input");
 const sendBtn = document.getElementById("send-btn");
 
-
-
-sendBtn.addEventListener("click",()=>{
+const sendMSG = () => {
     const data = {
         input: input.value,
         username: user.value,
         id: socket.id
     }
-    socket.emit("msg",data);
-});
+    socket.emit("msg", data);
+}
+
+sendBtn.addEventListener("click", sendMSG);
 //CHATROOM
-input.addEventListener("keydown",()=> socket.emit("typing",{user:username.value,isTyping:true}));
-input.addEventListener("keyup",()=> socket.emit("typing",{user:username.value,isTyping:false}));
-socket.on("msg",data =>msgs.innerHTML += `<p>${data.username}: ${data.input}</p>` );
-socket.on("typing",data =>terminal.innerHTML =(data.isTyping)? `<p>${data.user} is typing...</p>`: "" );
+input.addEventListener("keydown", e => { socket.emit("typing", { user: username.value, isTyping: true }); if (e.key == "Enter") sendMSG(); });
+input.addEventListener("keyup", () => socket.emit("typing", { user: username.value, isTyping: false }));
+socket.on("msg", data => msgs.innerHTML += `<p>${data.username}: ${data.input}</p>`);
+socket.on("typing", data => terminal.innerHTML = (data.isTyping) ? `<p>${data.user} is typing...</p>` : "");
