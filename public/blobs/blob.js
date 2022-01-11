42d3dbee3f84eff9974795fa27ba1a31ec9eb429
letuzo <martinlaguna1859@gmail.com>
2022-01-11 17:10:43 -0300

    last update 11/1

--- public/blobs/blob.js
+++ public/blobs/blob.js
@@ -1,8 +1,10 @@
 // const socket = io();
 
 const body = document.querySelector("body");
-const size = 50;
-let data, myBlob, stamina = true;
+
+let data, myBlob, stamina = true,
+    timer = 0,
+    usersInst = [];
 
 //init
 const mainBlob = data => { return `
@@ -25,10 +27,11 @@ window.addEventListener("scroll", () => window.scrollTo(0, 0));
 window.addEventListener("load", () => {
     data = {
         bg: Math.floor(Math.random() * 4096).toString(16),
-        x: Math.floor(Math.random() * (window.innerWidth - size)),
-        y: Math.floor(Math.random() * (window.innerHeight - size)),
+        x: Math.floor(Math.random() * (window.innerWidth - 50)),
+        y: Math.floor(Math.random() * (window.innerHeight - 50)),
         id: Math.floor(Math.random() * 4096).toString(16),
-        dir: 0
+        dir: 0,
+        size: 50
     }
     body.innerHTML += mainBlob(data);
     body.innerHTML += (`
@@ -39,6 +42,11 @@ window.addEventListener("load", () => {
 });
 socket.on("blob:initusers", users => {
     users.forEach(data => body.innerHTML += blob(data));
+});
+
+socket.on("blob:show-users", users => {
+    // console.log(users);
+    usersInst = users;
 });
 
 socket.on("blob:in", data => {
@@ -53,41 +61,79 @@ socket.on("blob:delete", user => {
 });
 
 const distance = {
-    0: () => { return [myBlob.offsetTop, 0, "top"] },
-    1: () => { return [window.innerWidth - (myBlob.offsetLeft + size), window.innerWidth - 5, "left"] },
-    2: () => { return [window.innerHeight - (myBlob.offsetTop + size), window.innerHeight + 3, "top"] },
-    3: () => { return [myBlob.offsetLeft + 3, 0, "left"] }
+    0: () => { return [myBlob.offsetTop, 0, "top", [0, 0.5]] },
+    1: () => { return [window.innerWidth - (myBlob.offsetLeft + data.size), window.innerWidth - 5, "left", [0.5, 1]] },
+    2: () => { return [window.innerHeight - (myBlob.offsetTop + data.size), window.innerHeight + 3, "top", [1, 0.5]] },
+    3: () => { return [myBlob.offsetLeft + 3, 0, "left", [0, 0]] }
+}
+
+const dist = (x0, y0, x1, y1) => {
+    return ((x1 - x0) ** 2 + (y0 - y1) ** 2) ** (1 / 2);
+}
+
+const intersect = (x0, y0, x1, y1) => {
+    let bulletDist = dist(x0, y0, x1, y1);
+    // console.log(bulletDist);
+    return bulletDist < data.size * 0.7;
+}
+
+const setShoot = async(b, own) => {
+    const bullet = `<div id="${b.id}" class="bullet" style="left:${b.left}px; top:${b.top}px; transition: all  ${b.dt}s linear;"></div>`
+    body.innerHTML += bullet;
+    let myBullet = document.getElementById(b.id);
+
+    await setTimeout(() => myBullet.style[b.axis] = b.limit + "px", 100);
+    let loop = setInterval(() => {
+        socket.emit("blob:get-users");
+        let found = usersInst.find(u => intersect(myBullet.offsetLeft, myBullet.offsetTop, u.x, u.y));
+        if (found) socket.emit("blob:decrease", found);
+    }, 100);
+
+    await setTimeout(() => {
+        if (own) stamina = true;
+        body.removeChild(myBullet)
+        clearInterval(loop);
+    }, b.dt * 1000);
+
+    return myBullet;
 }
 async function shoot() {
     stamina = false;
     myBlob = document.getElementById(data.id);
 
-    const vel = 100,
+    const vel = 400,
         bulletId = Math.floor(Math.random() * 10 ** 8);
 
-    const [dist, limit, axis] = distance[data.dir]();
+    const [dist, limit, axis, corr] = distance[data.dir]();
 
-    console.log(dist, limit, axis);
-    const bullet = `<div id="${bulletId}" class="bullet" style="left:${myBlob.offsetLeft+size/2}px; top:${myBlob.offsetTop+size/2}px; transition: all  ${dist/vel}s linear;"></div>`
-    body.innerHTML += bullet;
-    const myBullet = document.getElementById(bulletId);
+    bulletData = {
+        id: bulletId,
+        left: myBlob.offsetLeft + data.size * corr[1] + 10,
+        top: myBlob.offsetTop + data.size * corr[0] + 10,
+        dt: dist / vel,
+        limit,
+        axis
+    }
 
-    await setTimeout(() => myBullet.style[axis] = limit + "px", 100);
-    await setTimeout(() => { stamina = true;
-        body.removeChild(myBullet) }, (dist / vel) * 1000);
-    // socket.emit("blob:shoot",data);
+    // console.log(dist, limit, axis);
+    socket.emit("blob:shoot", bulletData);
+
+    await setShoot(bulletData, true);
 }
 
-
+socket.on("blob:shoot", bullet => setShoot(bullet));
+socket.on("blob:decrease", user => {
+    // let bloby = document.getElementById(user.id);
+    console.log(user.size);
+    // if (data.id == user.id) data = user;
+    // bloby.style.width = user.size + "px";
+    // bloby.style.height = user.size + "px";
+});
 //Blob Movement
 body.addEventListener("keydown", e => {
     const myBlob = document.getElementById(data.id);
 
-    //movement
-    // let left = parseInt(myBlob.style.left.substring(0, myBlob.style.left.length - 2));
-    // let top = parseInt(myBlob.style.top.substring(0, myBlob.style.top.length - 2));
-
-    const vel = 5;
+    const vel = 20;
     if (e.key == "w") myBlob.style.top = myBlob.offsetTop - vel + "px";
     if (e.key == "a") myBlob.style.left = myBlob.offsetLeft - vel + "px";
     if (e.key == "s") myBlob.style.top = myBlob.offsetTop + vel + "px";
@@ -95,6 +141,7 @@ body.addEventListener("keydown", e => {
 
     data.x = myBlob.offsetLeft;
     data.y = myBlob.offsetTop;
+
 
     //target
     if (e.key == "ArrowUp") data.dir = 0;
@@ -116,5 +163,3 @@ socket.on("blob:move", data => {
     blob.style.left = data.x + "px";
     blob.style.transform = `rotate(${(360/4)*data.dir}deg)`;
 });
-
-//Blob shooting
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/tasvivo",(req,res)=>res.json("staying alive"));
-const server = app.listen(PORT,()=>{console.log("listening on port 3000")});
+app.get("/tasvivo", (req, res) => res.json("staying alive"));
+const server = app.listen(PORT, () => { console.log("listening on port 3000") });
 
 const SocketIO = require("socket.io");
 const io = SocketIO(server);
@@ -16,47 +16,58 @@ const io = SocketIO(server);
 let users = [];
 ////////////////
 
-io.on("connection", socket=>{
-    
+io.on("connection", socket => {
+
     const _id = socket.id
 
     //CHATROOM
-    socket.on("msg", data =>{ 
-        io.sockets.emit("msg",data);
+    socket.on("msg", data => {
+        io.sockets.emit("msg", data);
         console.log(data);
     });
 
-    socket.on("typing", data =>{ 
-        socket.broadcast.emit("typing",data);
+    socket.on("typing", data => {
+        socket.broadcast.emit("typing", data);
     });
 
     //BLOBS
-    socket.on("blob:in", data =>{ 
-        // console.log(data);
-        socket.broadcast.emit("blob:in",data);
-        socket.emit("blob:initusers",users);
+    socket.on("blob:in", data => {
+        socket.broadcast.emit("blob:in", data);
+        socket.emit("blob:initusers", users);
         data["uid"] = socket.id;
         users.push(data);
     });
 
-    socket.on("blob:move", data =>{ 
-        socket.broadcast.emit("blob:move",data);
+    socket.on("blob:move", data => {
+        socket.broadcast.emit("blob:move", data);
         let user = users[users.indexOf(users.filter(u => u.uid == _id)[0])];
-        user.x = data.x; user.y = data.y;
+        user.x = data.x;
+        user.y = data.y;
         user.dir = data.dir;
     });
 
-    socket.on("blob:shoot", data =>{ 
-        socket.broadcast.emit("blob:shoot",data);
-        console.log(data.id + " shoot");
+    socket.on("blob:shoot", data => {
+        let user = users[users.indexOf(users.filter(u => u.uid == _id)[0])];
+        user.size -= 5;
+        socket.broadcast.emit("blob:shoot", user);
+        // console.log(data + " shoot");
+    });
+    socket.on("blob:decrease", data => {
+
+        socket.emit("blob:decrease", data);
     });
 
-    socket.on("blob:users", () =>{ 
-       console.log(users);
+    //handle data and disconections
+
+    socket.on("blob:users", () => {
+        console.log(users);
     });
+    socket.on("blob:get-users", () =>
+        socket.emit("blob:show-users", users)
+    );
     socket.on('disconnect', function() {
         console.log(_id);
-        socket.broadcast.emit("blob:delete",users.filter(u => u.uid==_id)[0]);
-        users = users.filter(u => u.uid!=_id);
+        socket.broadcast.emit("blob:delete", users.filter(u => u.uid == _id)[0]);
+        users = users.filter(u => u.uid != _id);
     });
 })
